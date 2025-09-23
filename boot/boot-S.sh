fa7015ce12942b42f27e78dd66f0d6080e132f27
Runkuan <leaker233@icloud.com>
2025-09-23 02:17:21 +0000

    Boot qemu for arm64

---
+++ boot/boot-S.sh
@@ -0,0 +1,13 @@
+KERNEL="../build/arch/arm64/boot/Image"
+DRIVE="./bullseye.img"
+MNTPATH="./mnt_path/"
+
+sudo qemu-system-aarch64 -s -S \
+    -machine virt -enable-kvm -cpu host \
+    -smp 1 -m 16G \
+    -kernel "$KERNEL" \
+    -drive file="$DRIVE",if=virtio,format=raw \
+    -fsdev local,path="$MNTPATH",security_model=mapped,id=dev-1 \
+    -device virtio-9p,fsdev=dev-1,mount_tag=mount-1 \
+    -nographic \
+    -append "kaslr console=ttyAMA0 root=/dev/vda"
