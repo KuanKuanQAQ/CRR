6296565a67b300c7dbdb23b64ef28fcc60083b0f
Runkuan <leaker233@icloud.com>
2025-09-29 05:42:14 +0000

    Network qemu

--- boot/boot.sh
+++ boot/boot.sh
@@ -3,11 +3,15 @@ DRIVE="./bullseye.img"
 MNTPATH="./mnt_path/"
 
 sudo qemu-system-aarch64 -s \
-    -machine virt -enable-kvm -cpu host \
-    -smp 1 -m 16G \
+    -machine virt -cpu cortex-a76 \
+    -smp 4 -m 16G \
     -kernel "$KERNEL" \
     -drive file="$DRIVE",if=virtio,format=raw \
     -fsdev local,path="$MNTPATH",security_model=mapped,id=dev-1 \
     -device virtio-9p,fsdev=dev-1,mount_tag=mount-1 \
+    -net nic -net tap,ifname=tap0,script=no,downscript=no \
     -nographic \
-    -append "kaslr console=ttyAMA0 root=/dev/vda"
+    -append "nokaslr console=ttyAMA0 root=/dev/vda earlyprintk=serial ramdisk_size=2097152"
+
+# -machine virt -cpu host -enable-kvm \
+# -machine virt -cpu cortex-a76 \
